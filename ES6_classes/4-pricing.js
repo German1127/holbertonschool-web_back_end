7eb7c1e2e5940fa209f2edc55a036b3b762f5119
German1127 <kleverk2023@gmail.com>
2024-04-29 13:26:05 -0700

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+    constructor(amount, currency) {
+      if (typeof (currency) === 'object') { this._currency = currency; }
+      if (typeof (amount) === 'number') { this._amount = amount; }
+    }
+  
+    static convertPrice(amount, conversionRate) {
+      return amount * conversionRate;
+    }
+  
+    set amount(amount) {
+      if (typeof (amount) === 'number') { this._amount = amount; }
+    }
+  
+    get amount() {
+      return this._amount;
+    }
+  
+    set currency(currency) {
+      if (typeof (currency) === 'object') { this._currency = currency; }
+    }
+  
+    get currency() {
+      return this._currency;
+    }
+  
+    displayFullPrice() {
+      return (`${this._amount} ${this.currency._name} (${this.currency._code})`);
+    }
+  }
