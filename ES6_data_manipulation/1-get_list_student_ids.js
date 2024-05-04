d389563fc074769b1505be3e8ff580c18827402a
German1127 <kleverk2023@gmail.com>
2024-05-04 05:54:03 -0700

    task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,11 +2,11 @@
 
 const getListStudentIds = (ids) => {
     if (!Array.isArray(ids)) {
-      return [];
+        return [];
     }
     const meids = ids.map((item) => item.id);
 
     return meids;
-  };
+    };
 
     export default getListStudentIds;
